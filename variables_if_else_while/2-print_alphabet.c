6e774683cc2a8a3ce9dcc4d6c80823c8bbf04d6c
Xdamix1991 <bourichalamine1@gmail.com>
2024-06-18 13:48:37 +0000

    alpha while

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+/**
+*main - return nothing
+*Description: 'putchar'
+*Return: Always 0 (Success)
+*/
+#include <stdio.h>
+int main(void)
+{
+char ch = 'a';
+while (ch <= 26)
+{putchar(ch);
+ch++; }
+return (0);
+}
