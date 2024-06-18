2c0e2be88b88ab0d076097081b3387642d48fe4a
Xdamix1991 <bourichalamine1@gmail.com>
2024-06-18 17:48:33 +0000

    for if loop

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+/*
+*main - Prints alphabets from 'a' to 'z', except 'q' and 'e'
+*Description: 'This function uses a for loop to iterate through the alphabet'
+*Return: Always 0 (success)
+*/
+#include <stdio.h>
+int main(void)
+{
+char lch = 'a';
+for (lch = 'a'; lch <= 'z'; lch++)
+{
+if (lch == 'q' || lch == 'e')
+continue;
+putchar(lch);
+}
+putchar('\n');
+return (0);
+}
