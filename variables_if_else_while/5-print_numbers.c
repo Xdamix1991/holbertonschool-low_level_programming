ead70e0a886fae645c6d67151897617cf87acf51
Xdamix1991 <bourichalamine1@gmail.com>
2024-06-18 20:11:14 +0000

    print numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+/**
+*main - return digit numbers
+*Description: 'printf numbers'
+*Return: Always 0 (Success)
+*/
+#include <stdio.h>
+int main(void)
+{
+int n;
+n = 0;
+while (n < 10)
+{ printf("%d", n);
+n++;
+}
+return (0);
+}
+
