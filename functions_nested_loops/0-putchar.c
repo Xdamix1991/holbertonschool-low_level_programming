b10b84481dcbdf173eececcc12c34b4ffb0b6b1e
Xdamix1991 <bourichalamine1@gmail.com>
2024-06-19 15:57:17 +0000

    putchar fonction

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -3,10 +3,16 @@
 *Description: 'printf'
 *Return: Always 0 (Success)
 */
-#include <stdio.h>
 #include "main.h"
 int main(void)
 {
-printf("_putchar\n");
+_putchar ('_');
+_putchar ('p');
+_putchar ('u');
+_putchar ('t');
+_putchar ('c');
+_putchar ('a');
+_putchar ('r');
+_putchar ('\n');
 return (0);
 }
