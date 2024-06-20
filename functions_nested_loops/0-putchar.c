0d4ab355f519bd250293554b8f9f867daf0e7d47
Xdamix1991 <bourichalamine1@gmail.com>
2024-06-20 11:08:03 +0000

    0-putchar.c

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -3,16 +3,17 @@
 *Description: 'printf'
 *Return: Always 0 (Success)
 */
+#include <unistd.h>
 #include "main.h"
 int main(void)
 {
-_putchar ('_');
-_putchar ('p');
-_putchar ('u');
-_putchar ('t');
-_putchar ('c');
-_putchar ('a');
-_putchar ('r');
-_putchar ('\n');
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 return (0);
 }
