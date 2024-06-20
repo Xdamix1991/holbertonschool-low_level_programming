18c3ff28b350fb2b585a21e983be0b520216105d
Xdamix1991 <bourichalamine1@gmail.com>
2024-06-20 11:55:05 +0000

    1-alphabet.c

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+/**
+*main - return nothin
+*Description: 'printf'
+*Return: Always 0 (Success)
+*/
+#include <unistd.h>
+#include "main.h"
+int main(void)
+{
+print_alphabet();
+_putchar('\n');
+return (0);
+}
