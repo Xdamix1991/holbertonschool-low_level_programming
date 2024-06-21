0854d0f9d0063f666790ba343525911604fd1247
Xdamix1991 <bourichalamine1@gmail.com>
2024-06-21 14:39:14 +0000

    7-print_last_digit.c

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,12 +1,20 @@
 /**
 *print_last_digit - print last number
 *@n: The number to print
+*@v: The variable to print
 *Description: print the last number
 *Return: Always 0 (Success).
 *On error, -1 is returned, and errno is set appropriately.
 */
 #include "main.h"
-int print_last_digit(int n)
+int print_last_digit(int n, v)
 {
-return (n % 10);
+int v;
+v = n % 10;
+if (v < 0)
+{
+v = v * -1;
+}
+_putchar(v + '0');
+return (v);
 }
