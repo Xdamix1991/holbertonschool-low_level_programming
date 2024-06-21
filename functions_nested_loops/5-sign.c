cfa89b097f55b4f83eb8ef7a6290b0829fe7f9a5
Xdamix1991 <bourichalamine1@gmail.com>
2024-06-21 12:21:23 +0000

    5-sign.c

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+/**
+*print_sign - print sign of a number
+*@c: The character to print
+*Description: print the sign
+*Return: Always 0 (Success).
+*On error, -1 is returned, and errno is set appropriately.
+*/
+#include "main.h"
+int print_sign(int n)
+{
+if (n > 1)
+{_putchar('+');
+return (1);
+}
+else if (n < 0)
+{_putchar('-');
+return (-1);
+}
+else
+{_putchar('0');
+return (0);
+}
+}
