cfb4ae710a6629fd864f97974b914eb0e4309e47
Xdamix1991 <bourichalamine1@gmail.com>
2024-06-22 17:01:31 +0000

    9tbale

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,26 @@
+/**
+*times_table - print table of nine
+*Description: print the table
+*Return: Always 0 (Success).
+*On error, -1 is returned, and errno is set appropriately.
+*/
+#include "main.h"
+void times_table(void)
+{
+int A, B, C;
+A = 0;
+while (A < 10)
+{
+C = A *B;
+B = 0;
+while (B < 10)
+{
+_putchar (C + '0');
+_putchar(',');
+_putchar(' ');
+B++;
+_putchar('\n');
+}
+A++;
+}
+}
