4913b253a9a9641ae82aa183ead708b216161b88
Xdamix1991 <bourichalamine1@gmail.com>
2024-06-22 21:03:52 +0000

    10-add.c

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+/**
+*add - print table of nine
+*Description: print the add of two numbers
+*@A: The number to add
+*@B: The number to add
+*Return: Always 0 (Success).
+*On error, -1 is returned, and errno is set appropriately.
+*/
+#include "main.h"
+int add(int A, int B)
+{
+int A, B;
+A = 0;
+B = 0;
+return (A + B);
+}
