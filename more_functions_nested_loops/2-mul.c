efe99caff3164d71d4ad6bddcb3b2b4742d4d8c9
Xdamix1991 <bourichalamine1@gmail.com>
2024-06-24 08:47:35 +0000

    2-mul.c

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+/**
+*mul - check for upper character
+*Description: funcction to do multiplications
+*@a: The digit to print
+*@b: the digit to print
+*Return: Always 0 (Success).
+*On error, -1 is returned, and errno is set appropriately.
+*/
+#include "main.h"
+int mul(int a, int b)
+{
+return (a * b);
+}
