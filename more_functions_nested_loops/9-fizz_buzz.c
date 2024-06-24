9c462474e11208b5c75bb6f4e9ab1c7fbe52ba7e
Xdamix1991 <bourichalamine1@gmail.com>
2024-06-24 20:54:55 +0000

    9-fizz_buzz.c

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -15,11 +15,11 @@ if (i % 3 == 0  && i % 5 == 0)
 {
 printf("FizzBuzz");
 }
-else if(i % 3 == 0)
+else if (i % 3 == 0)
 {
 printf("Fizz");
 }
-else if(i % 5 == 0)
+else if (i % 5 == 0)
 {
 printf("Buzz");
 }
@@ -27,7 +27,7 @@ else
 {
 printf("%d", i);
 }
-if (i != 100) 
+if (i != 100)
 {
 printf(" ");
 }
