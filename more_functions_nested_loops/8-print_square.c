7b57964104ff7f30d971e5ac435fb52a66b73e9d
Xdamix1991 <bourichalamine1@gmail.com>
2024-06-24 14:02:14 +0000

    8-print_square.c

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,32 @@
+/**
+*print_square - print char number of time
+*Description: print char
+*@size: print line
+*Return: Always 0 (Success).
+*On error, -1 is returned, and errno is set appropriately.
+*/
+#include "main.h"
+void print_square(int size)
+{
+if (size > 0)
+{
+int i;
+{
+for (i = 0; i < size; i++)
+{
+int j;
+for (j = 0; i < j; j++)
+{
+_putchar(35);
+}
+_putchar(35);
+_putchar('\n');
+}
+_putchar('\n');
+}
+}
+else
+{
+_putchar('\n');
+}
+}
