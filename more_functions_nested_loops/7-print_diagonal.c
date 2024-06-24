5932ee01ff362fa2a3207b318f1a857cf9ba00e8
Xdamix1991 <bourichalamine1@gmail.com>
2024-06-24 13:27:41 +0000

    7-print_diagonal.c

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+/**
+*print_diagonal - print a diagonal
+*Description: print the time
+*@n: number to print
+*Return: Always 0 (Success).
+*On error, -1 is returned, and errno is set appropriately.
+*/
+#include "main.h"
+void print_diagonal(int n)
+{
+if (n > 0)
+{
+int i;
+for (i = 0; i < n; i++)
+{
+_putchar(92);
+}
+_putchar('\n');
+}
+else
+{
+_putchar('\n');
+}
+}
