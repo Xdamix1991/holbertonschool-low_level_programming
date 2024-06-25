f3e0d1cfe0857c3786374962c78df0eb59fcdcf9
Xdamix1991 <bourichalamine1@gmail.com>
2024-06-25 10:43:09 +0000

    10-print_triangle.c

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+/**
+*print_triangle - print char number of time
+*Description: print triangle
+*@size: rectangle to print
+*Return: Always 0 (Success).
+*On error, -1 is returned, and errno is set appropriately.
+*/
+#include "main.h"
+void print_triangle(int size)
+{
+if (size > 0)
+{
+int i;
+for (i = 0; i < size; i++)
+{
+int j;
+for (j = 0; j < size - i - 1; j++)
+{
+_putchar(' ');
+}
+for (j = 0; j <= size; j++)
+{
+_putchar('#');
+}
+}
+_putchar('\n');
+}
+else
+_putchar('\n');
+}
