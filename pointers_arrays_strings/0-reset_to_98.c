08a13f2e191f1251fd41d2700105073a476a1cce
Xdamix1991 <bourichalamine1@gmail.com>
2024-06-25 13:26:21 +0000

    0-reset_to_98.c

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+/**
+*reset_to_98 - update the value
+*Description: print triangle
+*@n: pointer
+*Return: Always 0 (Success).
+*On error, -1 is returned, and errno is set appropriately.
+*/
+#include "main.h"
+void reset_to_98(int *n)
+{
+int a;
+{
+*n = 98;
+n = &a;
+}
+}
