a15211a86c8187b6f9bc435f5900c96defdcee49
Xdamix1991 <bourichalamine1@gmail.com>
2024-06-26 19:22:46 +0000

    8-print_array.c

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+/**
+*print_array - print the value of an array
+*Description: up
+*@a: pointer
+*@n: array value
+*Return: Always 0 (Success).
+*On error, -1 is returned, and errno is set appropriately.
+*/
+#include <stdio.h>
+#include "main.h"
+void print_array(int *a, int n)
+{
+for (int x = 0; x < n; x++)
+{
+printf("%d", a[x]);
+if (x != n - 1)
+{
+printf(", ");
+}
+}
+printf("\n");
+}
