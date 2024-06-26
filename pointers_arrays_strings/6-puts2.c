2665c00c6966b499ee662512e153b3160a6a6769
Xdamix1991 <bourichalamine1@gmail.com>
2024-06-26 14:05:26 +0000

    6-puts2.c

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+/**
+*puts2 - print string in reverse
+*Description: reverse string
+*@str: pointer
+*Return: Always 0 (Success).
+*On error, -1 is returned, and errno is set appropriately.
+*/
+#include "main.h"
+void puts2(char *str)
+{
+int x = 0;
+char print;
+while (str[x] != '\0')
+{
+_putchar(str[x]);
+_putchar('\n');
+x++;
+}
+print = str[x];
+}
+
