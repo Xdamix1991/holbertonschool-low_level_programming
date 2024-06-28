298d0e8d992f4c207d27a269c7bdab51cfd37de8
Xdamix1991 <bourichalamine1@gmail.com>
2024-06-28 11:07:43 +0000

    0-strcat.c

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -10,16 +10,15 @@
 char *_strcat(char *dest, char *src)
 {
 int x = 0;
+int y;
 while (dest[x] != '\0')
 {
 x++;
 }
-int y = 0;
-while (src[y] != '\0')
+for (y = 0; src[y] != '\0'; y++)
 {
 dest[x] = src[y];
 x++;
-y++;
 }
 dest[x] = '\0';
 return (dest);
