8d4f38c5d7cbfeeefc03987489f46ce089d7a46f
Xdamix1991 <bourichalamine1@gmail.com>
2024-06-28 11:26:27 +0000

    1-strncat.c

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+/**
+*_strncat - add string to another
+*Description: up
+*@dest: pointer
+*@src: pointer
+*Return: Always 0 (Success).
+*On error, -1 is returned, and errno is set appropriately.
+*/
+#include "main.h"
+char *_strncat(char *dest, char *src, int n)
+{
+int x = 0;
+int y;
+while (dest[x] != '\0')
+{
+x++;
+}
+for (y = 0; src[y] != '\0' || y == n; y++)
+{
+dest[x] = src[y];
+x++;
+}
+dest[x] = '\0';
+return (dest);
+}
