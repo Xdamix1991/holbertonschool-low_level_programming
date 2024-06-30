6cfb4f132a6f3df2471e12d32450e824b107449c
Xdamix1991 <bourichalamine1@gmail.com>
2024-06-30 10:51:12 +0000

    2-strncpy.c

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -11,17 +11,15 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 int i = 0;
-int j;
-while (src[i] != '\0')
-while (dest[i] != '\0')
+while (src[i] != '\0' && i < n)
 {
-dest[i] = src[j];
-for (j = 0; src[j] != '\0'; j++)
-{
-if (j < n)
-dest[i] = '\0';
+dest[i] = src[i];
 i++;
 }
+while (i < n)
+{
+dest[i] = '\0';
+i++;
 }
 return (dest);
 }
