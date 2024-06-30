05a88db2af41cb66ca5837dd35108655ed05608e
Xdamix1991 <bourichalamine1@gmail.com>
2024-06-30 12:18:09 +0000

    4-rev_array.c

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,31 @@
+/**
+*reverse_array - copie strings
+*Description: function that copies strings
+*@a: pointer
+*@n: variable
+*Return: Always 0 (Success).
+*On error, -1 is returned, and errno is set appropriately.
+*/
+#include "main.h"
+void reverse_array(int *a, int n)
+{
+int i = 0;
+int j = 0;
+int temp;
+while (i < n)
+{
+i++;
+}
+i = n - 1;
+{
+while (j < n)
+{
+temp = a[i];
+a[i] = a[j];
+a[j] = temp;
+j++;
+i--;
+}
+}
+}
+
