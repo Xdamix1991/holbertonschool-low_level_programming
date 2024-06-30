606807f27b5d660c2a97d1753e22a0a5da4d8629
Xdamix1991 <bourichalamine1@gmail.com>
2024-06-30 10:39:16 +0000

    2-strncpy.c

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+/**
+*_strncpy - copie strings
+*Description: function that copies strings
+*@dest: pointer
+*@src: pointer
+*@n: number of array
+*Return: Always 0 (Success).
+*On error, -1 is returned, and errno is set appropriately.
+*/
+#include "main.h"
+char *_strncpy(char *dest, char *src, int n)
+{
+int i = 0;
+int j;
+while (src[i] != '\0')
+while (dest[i] != '\0')
+{
+dest[i] = src[j];
+for (j = 0; src[j] != '\0'; j++)
+{
+if (j < i)
+dest[i] = '\0';
+i++;
+}
+}
+return (dest);
+}
