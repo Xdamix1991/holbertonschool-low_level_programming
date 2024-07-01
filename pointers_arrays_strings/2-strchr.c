c1ae6dc8e13ac3786f829e669f45bf2790b418ec
Xdamix1991 <bourichalamine1@gmail.com>
2024-07-01 12:37:12 +0000

    2-strchr.c

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+/**
+**_strchr - function that fills memory with a constant byte
+*Description: constant byte
+*@s: pointer
+*@c: char
+*Return: Always 0 (Success).
+*On error, -1 is returned, and errno is set appropriately.
+*/
+#include "main.h"
+char *_strchr(char *s, char c)
+{
+int i = 0;
+while (s[i] != '\0')
+{
+if (s[i] == c)
+{
+return (&s[i]);
+}
+i++;
+}
+if (c == '\0')
+i{
+return (&s[i]);
+}
+return (NULL);
+}
