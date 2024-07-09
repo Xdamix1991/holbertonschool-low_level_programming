0bde6ac372ee6e3f7d13b0733095d47b9dbc5ea2
Xdamix1991 <bourichalamine1@gmail.com>
2024-07-09 13:51:16 +0000

    0-malloc_checked.c

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+/**
+*malloc_checked - function function that allocates memory using malloc
+*@b: size of integer
+*Return: On success 1.
+*On error, 1 is returned, and errno is set appropriately.
+**/
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+void *malloc_checked(unsigned int b)
+{
+void *s;
+s = malloc(sizeof(unsigned int));
+if (s == NULL)
+{
+exit (98);
+}
+return (s);
+}
