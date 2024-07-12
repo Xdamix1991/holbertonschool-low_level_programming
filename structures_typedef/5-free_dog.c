f2f3ad59bde540c8db2d5a3bc358679e738ccfe0
Xdamix1991 <bourichalamine1@gmail.com>
2024-07-12 14:59:27 +0000

    5-free_dog.c

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,24 @@
+/**
+*free_dog - function to free a variable of type dog_t
+*@d: Pointer to type dog_t
+*Return: void return
+**/
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+void free_dog(dog_t *d)
+{
+if (d == NULL)
+{
+return;
+}
+if (d->name != NULL)
+{
+free(d->name);
+}
+if (d->owner != NULL)
+{
+free(d->owner);
+}
+free(d);
+}
