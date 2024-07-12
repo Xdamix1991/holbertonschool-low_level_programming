cf87c60181f1a856ee73617708b25c572a7c0e73
Xdamix1991 <bourichalamine1@gmail.com>
2024-07-12 11:07:07 +0000

    dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -10,13 +10,13 @@
 * including its name, age, and its owner's name.
 **/
 
-struct dog
+typedef struct dog
 {
 char *name;
 float age;
 char *owner;
-};
+}dog_t;
+
 /* more prototypes */
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
+
 #endif
