c5230447d7817d433e2e048b109fe4523cde5315
Xdamix1991 <bourichalamine1@gmail.com>
2024-07-12 11:38:25 +0000

    dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -20,5 +20,6 @@ char *owner;
 /* more prototypes */
 void init_dog(dog_t *d, char *name, float age, char *owner);
 void print_dog(dog_t *d);
+dog_t *new_dog(char *name, float age, char *owner);
 
 #endif /* DOG_H*/
