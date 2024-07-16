eac4d80413b7bfcad6a58e7fa2ae0a89e25b3939
Xdamix1991 <bourichalamine1@gmail.com>
2024-07-16 07:37:12 +0000

    task3

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -1,25 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-/** addion **/
+/**
+*op_add - Adds two integers
+*@a: The first integer
+*@b: The second integer
+*Return: The sum of a and b
+*addion **/
 int op_add(int a, int b)
 {
 return (a + b);
 }
-
-/** subtraction **/
+/**
+*op_sub - substract two integers
+*@a: The first integer
+*@b: The second integer
+*Return: The div of a and b
+*subtraction **/
 int op_sub(int a, int b)
 {
 return (a - b);
 }
-
-/** multiplication **/
+/**
+*op_mul - multiplies two integers
+*@a: The first integer
+*@b: The second integer
+*Return: The mul of a and b
+*multiplication **/
 int op_mul(int a, int b)
 {
 return (a * b);
 }
-
-/** division **/
+/**
+*op_div - Adds two integers
+*@a: The first integer
+*@b: The second integer
+*Return: The div of a and b
+*division **/
 int op_div(int a, int b)
 {
 if (b == 0)
@@ -29,8 +45,12 @@ exit(100);
 }
 return (a / b);
 }
-
-/** modulo **/
+/**
+*op_mod - Adds two integers
+*@a: The first integer
+*@b: The second integer
+*Return: The modulo of a / b
+*modulo **/
 int op_mod(int a, int b)
 {
 if (b == 0)
@@ -40,4 +60,3 @@ exit(100);
 }
 return (a % b);
 }
-
