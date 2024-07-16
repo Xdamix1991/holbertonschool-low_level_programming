8017e2967b510c20c5cf7637d1ff41b69c0a9087
Xdamix1991 <bourichalamine1@gmail.com>
2024-07-16 10:30:04 +0000

    0-print_name.c

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include "function_pointers.h"
 /**
 *print_name - Prints a name using a specified processing function.
 *@name: Pointer to a string containing the name to print.
