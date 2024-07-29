4b6daea669ec8e3252b3ee858792c718625edf14
Xdamix1991 <bourichalamine1@gmail.com>
2024-07-29 13:29:37 +0000

    fixed requirements problems

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -2,6 +2,49 @@
 #include <string.h>
 #include <stdio.h>
 #include "lists.h"
+
+/**
+ * my_strlen - Calculates the length of a string
+ * @str: The string whose length is to be calculated
+ *
+ * Return: The length of the string
+ */
+
+size_t my_strlen(const char *str)
+{
+	size_t len;
+
+	len = 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	return (len);
+}
+
+/**
+ * my_strcpy - Copies a string
+ * @dest: The destination buffer to copy the string to
+ * @src: The source string to be copied
+ *
+ * Return: A pointer to the destination string
+ */
+char *my_strcpy(char *dest, const char *src)
+{
+	size_t i:
+
+	i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
 /**
  * add_node - Adds a new node at the beginning of a list_t list.
  * @head: A pointer to the pointer to the head of the list.
@@ -17,9 +60,10 @@ list_t *add_node(list_t **head, const char *str)
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 	{
+		free(new_node);
 		return (NULL);
 	}
-	str_len = strlen(str);
+	str_len = my_strlen(str);
 
 	new_node->len = str_len;
 
@@ -30,11 +74,10 @@ list_t *add_node(list_t **head, const char *str)
 		free(new_node);
 		return (NULL);
 	}
-	strcpy(new_node->str, str);
+	my_strcpy(new_node->str, str);
 
 	new_node->next = *head;
 	*head = new_node;
 	return (new_node);
 }
-
 
