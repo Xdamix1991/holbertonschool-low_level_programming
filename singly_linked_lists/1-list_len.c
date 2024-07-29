d4abffc9a844ad526b66442a0f419254f6116698
Xdamix1991 <bourichalamine1@gmail.com>
2024-07-29 11:18:47 +0000

    1-list_len.c

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ *@list_len: function that prints number of elements in a linked list
+ *@h: a pointer to the structure
+ *Return: the number of nodes
+ */
+
+size_t list_len(const list_t *h)
+
+{
+	size_t i;
+
+	i = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
