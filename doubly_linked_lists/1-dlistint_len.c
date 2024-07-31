6d38625d63474ab1c3378a7ba2b6962ea82289c1
Xdamix1991 <bourichalamine1@gmail.com>
2024-07-31 07:18:16 +0000

    1-dlistint_len.c

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ *dlistint_len: function that prints number of elements in a linked list
+ *@h: a pointer to the structure
+ *Return: the number of nodes
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+
+{
+	size_t i;
+
+	i = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
+
